72a7b71e33997cd4c1ea927263e514c938ecd5b1
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-06-25 20:57:42 +0530

    Time: 616 ms (5.24%), Space: 17.4 MB (97.05%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.py
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.py
@@ -1,23 +1,14 @@
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
-        if t in s:
-            return 0
-        if len(t)==0:
-            return 0
-        if len(t)==1:
-            if t not in s:
-                return 1
-            else:
-                return 0
         n=len(t)
         m=len(s)
         k=0
         l=0
         count=0
-        split=-1
+        # split=-1
         while(l<m):
             if t[k] not in s[l:len(s)]:
-                split=k
+                # split=k
                 break
             if t[k]==s[l]:
                 k+=1
