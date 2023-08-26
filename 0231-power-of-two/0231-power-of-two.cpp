6e79e372df1bc5ee2a758edefd1bb15a29d503ea
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-08-26 15:38:25 +0530

    Time: 0 ms (100%), Space: 6.1 MB (15.38%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n){
-        if(n==0){
+        // if(n==0){
+        //     return false;
+        // }
+        // return ((n==1) || (n%2==0 && isPowerOfTwo(n/2)));
+        if(n<0){
             return false;
         }
-        return ((n==1) || (n%2==0 && isPowerOfTwo(n/2)));
+        int b = __builtin_popcount(n);
+        if(b==1){
+            return true;
+        }
+        return false;
     }
+
 };
