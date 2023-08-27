8918b2d112fbe779e874add72d0edf45f311efda
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-08-27 19:44:44 +0530

    Time: 4 ms (20.45%), Space: 7.6 MB (9.86%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string> mp;
+        unordered_map<string,int> cmp;
+        istringstream ss(s);
+        string word;
+        int i=0;
+        while(ss >> word){
+            mp[pattern[i]]=word;
+            cmp[word]++;
+            i++;
+        }
+        if(mp.size()!=cmp.size()){
+            return false;
+        }
+        string res="";
+        for(int i=0;i<pattern.size();i++){
+            if(i==pattern.size()-1){
+                res = res + mp[pattern[i]];
+                continue;
+            }
+            res = res + mp[pattern[i]] + " ";
+        }
+        // for(auto p : mp){
+        //     cout<<p.first<<" "<<p.second<<endl;
+        // }
+        cout<<res;
+        if(res==s){
+            return true;
+        }
+        return false;
+
+    }
+};
