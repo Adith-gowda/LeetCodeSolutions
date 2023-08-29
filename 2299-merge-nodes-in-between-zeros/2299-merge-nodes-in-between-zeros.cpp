4510d7852710f3ede60eb7242c90567a2f807ca2
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-08-29 08:35:31 +0530

    Time: 512 ms (93.81%), Space: 253.2 MB (76.57%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -10,20 +10,38 @@
  */
 class Solution {
 public:
+    void merge(ListNode* &temp,ListNode* &curr, int sum){
+        if(!temp){
+            return;
+        }
+        if(temp->val==0){
+            curr->val=sum;
+            sum=0;
+            curr->next = temp->next;
+            curr = curr->next;
+        }
+        if(!temp){
+            return;
+        }
+        sum = sum + temp->val;
+        merge(temp->next,curr,sum);
+
+    }
     ListNode* mergeNodes(ListNode* head){
         ListNode* temp = head->next;
         ListNode* curr=temp;
         int sum=0;
-        while(temp!=NULL){
-            if(temp->val==0){
-                curr->val=sum;
-                sum=0;
-                curr->next = temp->next;
-                curr = curr->next;
-            }
-            sum = sum + temp->val;
-            temp = temp->next;
-        }
+        // while(temp!=NULL){
+        //     if(temp->val==0){
+                // curr->val=sum;
+                // sum=0;
+                // curr->next = temp->next;
+                // curr = curr->next;
+        //     }
+            // sum = sum + temp->val;
+        //     temp = temp->next;
+        // }
+        merge(temp,curr,sum);
         return head->next;
 
     }
