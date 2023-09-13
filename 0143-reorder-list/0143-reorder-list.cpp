9c54721cde8fd3fdb74404235ae977718926c884
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-09-13 09:19:18 +0530

    Time: 37 ms (41.18%), Space: 17.8 MB (42.81%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* temp = head;
+        int len=0;
+        while(temp!=NULL){
+            len++;
+            temp=temp->next;
+        }
+
+        ListNode* first;
+        temp=head;
+        ListNode* second;
+
+        int k = len/2;
+        while(k){
+            first=temp;
+            temp = temp->next;
+            k--;
+        }
+        second = temp;
+        if(len%2!=0){
+            first=second;
+            second = second->next;
+        }
+        first->next=NULL;
+        // cout<<first->val<<" "<<second->val;
+
+        ListNode* prev = NULL;
+        ListNode* succ = NULL;
+
+        while(second!=NULL){
+            succ=second->next;
+            second->next=prev;
+            prev = second;
+            second = succ;
+        }
+
+        second = prev;
+
+        // while(second!=NULL){
+        //     cout<<second->val<<" ";
+        //     second=second->next;
+        // }
+        temp = head;
+        succ=NULL;
+        prev=NULL;
+
+        while(temp!=NULL && second!=NULL){
+            succ=temp->next;
+            temp->next=second;
+            prev=second->next;
+            second->next=succ;
+            temp=succ;
+            second=prev;
+        }
+    }
+};
