73addb2d396a47c2de0756ab188cd3b6a7e77619
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-09-14 18:44:14 +0530

    Time: 16 ms (59.59%), Space: 9.5 MB (5.19%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,12 +3,8 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> mp1;
         unordered_map<char,int> mp2;
-        for(int i=0;i<ransomNote.size();i++){
-            mp1[ransomNote[i]]++;
-        }
-        for(int i=0;i<magazine.size();i++){
-            mp2[magazine[i]]++;
-        }
+        for(int i=0;i<ransomNote.size();i++) mp1[ransomNote[i]]++;
+        for(int i=0;i<magazine.size();i++) mp2[magazine[i]]++;
         for(auto p : mp1){
             if(mp2[p.first]>=mp1[p.first]) continue;
             else return false;
