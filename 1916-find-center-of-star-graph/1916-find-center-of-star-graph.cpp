eceaea9000ce681ffbb0c107c1b55b5b503f8b6b
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-09-18 09:45:48 +0530

    Time: 153 ms (73.57%), Space: 67.8 MB (49.82%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,19 +1,28 @@
+// class Solution {
+// public:
+//     int findCenter(vector<vector<int>>& edges) {
+//         unordered_map<int,int> mp;
+//         int ele;
+//         int mx=INT_MIN;
+//         for(auto i : edges){
+//             mp[i[0]]++;
+//             mp[i[1]]++;
+//         }
+//         for(auto p : mp){
+//             if(mx<p.second){
+//                 ele = p.first;
+//                 mx = max(mx,p.second);
+//             }
+//         }
+//         return ele;
+//     }
+// };
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int,int> mp;
-        int ele;
-        int mx=INT_MIN;
-        for(auto i : edges){
-            mp[i[0]]++;
-            mp[i[1]]++;
-        }
-        for(auto p : mp){
-            if(mx<p.second){
-                ele = p.first;
-                mx = max(mx,p.second);
-            }
-        }
-        return ele;
+        if(edges[0][0] == edges[1][0]) return edges[0][0];
+        else if(edges[0][0] == edges[1][1]) return edges[0][0];
+
+        return edges[0][1];
     }
 };
