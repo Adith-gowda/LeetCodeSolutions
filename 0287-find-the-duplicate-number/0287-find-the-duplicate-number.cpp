1a8859fcbd5fc4ae99a17457e0c30d2e2bf7f682
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-09-19 13:08:30 +0530

    Time: 90 ms (56.37%), Space: 61.5 MB (28.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,10 +1,22 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for(int i=1;i<nums.size();i++){
+//             if(nums[i-1]==nums[i]) return nums[i];
+//         }
+//         return 0;
+//     }
+// };
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i-1]==nums[i]) return nums[i];
+        int i=0;
+        while (nums[i] > 0){
+            int tmp = nums[i];
+            nums[i] = 0;
+            i = tmp;
         }
-        return 0;
+        return i;
     }
 };
