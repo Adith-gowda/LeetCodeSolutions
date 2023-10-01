4afbd056bd5f1ba42b3b0ed2158b8700f1788383
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-10-01 11:22:40 +0530

    Time: 20 ms (27.72%), Space: 11.4 MB (39.36%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,29 +1,55 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         vector<string> v;
+//         string str="";
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]==' ') 
+//             {
+//                 v.push_back(str);
+//                 str="";
+//                 continue;
+//             }
+//             str+=s[i];
+//         }
+//         v.push_back(str);
+//         string ans="";
+
+//         for(int i=0;i<v.size();i++){
+//             reverse(v[i].begin(),v[i].end());
+//             if(i==v.size()-1){
+//                 ans+=v[i];
+//                 continue;
+//             }
+//             ans = ans + v[i] + " ";
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> v;
-        string str="";
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' ') 
-            {
-                v.push_back(str);
-                str="";
-                continue;
+        string ans = "";
+        int start=0;
+        for(int i =0; i<s.length(); i++){
+            if(s[i] == ' '){
+                int temp = i-1;
+                while(temp >= start){
+                    ans += s[temp];
+                    temp--;
+                }
+                ans += ' ';
+                start = i+1;
             }
-            str+=s[i];
+            
         }
-        v.push_back(str);
-        string ans="";
-
-        for(int i=0;i<v.size();i++){
-            reverse(v[i].begin(),v[i].end());
-            if(i==v.size()-1){
-                ans+=v[i];
-                continue;
-            }
-            ans = ans + v[i] + " ";
-        }
-
+        int temp = s.length()-1;
+        while(temp >= start){
+                    ans += s[temp];
+                    temp--;
+                }
         return ans;
+        
     }
 };
