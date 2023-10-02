06686fbbe45401c2f259e2c01a8f9d67cda8b185
Adith sagar gowda <95766897+Adith-gowda@users.noreply.github.com>
2023-10-02 22:39:27 +0530

    Time: 131 ms (98.93%), Space: 110.6 MB (97.33%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,23 +1,54 @@
+// class Solution {
+// public:
+//     bool ispalin(vector<int>& v, int i, int j) {
+//         if (i >= j) {
+//             return true;
+//         }
+//         if (v[i] != v[j]) {
+//             return false;
+//         }
+//         return ispalin(v, i + 1, j - 1);
+//     }
+
+//     bool isPalindrome(ListNode* head) {
+//         vector<int> v;
+//         ListNode* node = head;       
+//         while (node != nullptr) {
+//             v.push_back(node->val);
+//             node = node->next;
+//         }
+        
+//         return ispalin(v, 0, v.size() - 1);
+//     }
+// };
 class Solution {
 public:
-    bool ispalin(vector<int>& v, int i, int j) {
-        if (i >= j) {
-            return true;
-        }
-        if (v[i] != v[j]) {
-            return false;
-        }
-        return ispalin(v, i + 1, j - 1);
+bool isPalindrome(ListNode* head) {
+    ListNode* fast = head;
+    ListNode* slow = head;
+    ListNode* prev = nullptr;  
+    while (fast != nullptr && fast->next != nullptr) {
+        fast = fast->next->next;
+
+        // Reverse the first half of the list
+        ListNode* nextTemp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = nextTemp;
     }
 
-    bool isPalindrome(ListNode* head) {
-        vector<int> v;
-        ListNode* node = head;       
-        while (node != nullptr) {
-            v.push_back(node->val);
-            node = node->next;
-        }
-        
-        return ispalin(v, 0, v.size() - 1);
+    //  skip the middle node if odd number of nodes,
+    if (fast != nullptr) {
+        slow = slow->next;
     }
+    while (prev != nullptr && slow != nullptr) {
+        if (prev->val != slow->val) {
+            return false;
+        }
+        prev = prev->next;
+        slow = slow->next;
+    }
+
+    return true;
+}
 };
